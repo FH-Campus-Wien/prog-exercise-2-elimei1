0628604d0f10746193bcac051b07c99bcc82e49e
Elias Meisl <elias.meisl@stud.fh-campswien.ac.at>
2022-10-06 15:38:39 +0200

    first step

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -5,23 +5,91 @@ import java.util.Scanner;
 public class App {
 
     //todo Task 1
-    public void largestNumber(){
+    public void largestNumber() {
         // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        double num = scanner.nextDouble();
+        double largestNumber = Double.MAX_VALUE;
+
+        double d;
+        for (d = 0; d < num; d++) {
+            int current = scanner.nextInt();
+            if (current > largestNumber) {
+                largestNumber = current;
+            }
+
+        }
+        if (num <= 0) {
+            System.out.println("The largest number is " + largestNumber);
+        }
+
+
     }
 
     //todo Task 2
     public void stairs(){
         // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        if (i <= 0) {
+            System.out.println("Invalid number!");
+        }
+        System.out.println(" ");
+        int a;
+        for (a = 1; a <= i; ++a) {
+            System.out.println("i");
+        }
     }
 
     //todo Task 3
     public void printPyramid(){
         // input your solution here
+        int a = 6;
+        int b;
+        int c;
+        int d;
+    for (b = 1; b <= a; b++) {          // counts up until six lines are reached
+            for (c = 5; c >= b; c--)    //counts down until spaces have reached 1
+                System.out.print(" ");
+            for (d = 1; d < 2*b; d++)   // counts up until * have reached 2 times the line number (-1)
+                System.out.print("*");
+
+            System.out.print(System.lineSeparator());
+        }
     }
 
     //todo Task 4
     public void printRhombus(){
         // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        int h = scanner.nextInt();
+        char c = scanner.next().charAt(0);
+        if (h % 2 == 0) {
+            System.out.println("Invalid number!");
+        }
+        int b;
+        int d;
+        int e;
+        int g;
+        int f;
+        for (b = 1; b <= 4; b++) {          // without ASCII until now
+            for (d = 4 - b; d >= 1; d--) {
+                System.out.print(" ");
+            }
+            for (e = 1; e <= 2 * b - 1; e++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int a = 1; a <= 3; a++) {
+            for (g = 1; g <= a; g++) {
+                System.out.print(" ");
+            }
+            for (f = 5; f >= 2 * a - 1; f -= 1) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     //todo Task 5
